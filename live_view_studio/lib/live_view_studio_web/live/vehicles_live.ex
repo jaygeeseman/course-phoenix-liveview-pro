b8940c99cbae1f84a59412ecdbbb39e2f0f1fbf0
Jay Geeseman <geesemanj@gmail.com>
2023-12-10 07:14:53 +0000

    Repeat autocomplete for vehicle search

--- live_view_studio/lib/live_view_studio_web/live/vehicles_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/vehicles_live.ex
@@ -8,7 +8,8 @@ defmodule LiveViewStudioWeb.VehiclesLive do
       assign(socket,
         query: "",
         vehicles: [],
-        loading: false
+        loading: false,
+        matches: []
       )
 
     {:ok, socket}
@@ -18,7 +19,7 @@ defmodule LiveViewStudioWeb.VehiclesLive do
     ~H"""
     <h1>🚙 Find a Vehicle 🚘</h1>
     <div id="vehicles">
-      <form phx-submit="search">
+      <form phx-submit="search" phx-change="autocomplete">
         <input
           type="text"
           name="query"
@@ -27,12 +28,20 @@ defmodule LiveViewStudioWeb.VehiclesLive do
           autofocus
           autocomplete="off"
           readonly={@loading}
+          list="matches"
+          phx-debounce="250"
         />
 
         <button>
           <img src="/images/search.svg" />
         </button>
       </form>
+
+      <datalist id="matches">
+        <option :for={make_model <- @matches}>
+          <%= make_model %>
+        </option>
+      </datalist>
 
       <div :if={@loading} class="loader">Loading...</div>
 
@@ -53,6 +62,11 @@ defmodule LiveViewStudioWeb.VehiclesLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("autocomplete", %{"query" => prefix}, socket) do
+    socket = assign(socket, matches: Vehicles.suggest(prefix))
+    {:noreply, socket}
   end
 
   def handle_event("search", %{"query" => query}, socket) do
