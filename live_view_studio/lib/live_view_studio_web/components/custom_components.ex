cbfeeae198ebc2ada92ded1b31fff0539d53841d
Jay Geeseman <geesemanj@gmail.com>
2023-12-12 02:35:30 +0000

    Change loading indicator to a spinner

--- live_view_studio/lib/live_view_studio_web/components/custom_components.ex
+++ live_view_studio/lib/live_view_studio_web/components/custom_components.ex
@@ -26,7 +26,12 @@ defmodule LiveViewStudioWeb.CustomComponents do
 
   def loader(assigns) do
     ~H"""
-    <div :if={@loading} class="loader">Loading...</div>
+    <div :if={@loading} class="flex justify-center my-10 relative">
+      <div class="w-12 h-12 rounded-full absolute border-8 border-gray-300">
+      </div>
+      <div class="w-12 h-12 rounded-full absolute border-8 border-indigo-400 border-t-transparent animate-spin">
+      </div>
+    </div>
     """
   end
 end
