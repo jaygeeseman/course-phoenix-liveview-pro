f04e379e9d4f4e1ab094a10c465feb17fd5f2fcf
Jay Geeseman <geesemanj@gmail.com>
2023-12-16 06:43:47 +0000

    Remove redundant query

--- live_view_studio/lib/live_view_studio_web/live/donations_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/donations_live.ex
@@ -4,13 +4,6 @@ defmodule LiveViewStudioWeb.DonationsLive do
   alias LiveViewStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
-
-    socket =
-      assign(socket,
-        donations: donations
-      )
-
     {:ok, socket}
   end
 
