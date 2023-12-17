0db1f1e63e04de66490840c415ebebd4be7fb1d4
Jay Geeseman <geesemanj@gmail.com>
2023-12-17 07:39:19 +0000

    Bonus exercise: Handle multiple URLs with live actions

--- live_view_studio/lib/live_view_studio_web/live/servers_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/servers_live.ex
@@ -14,7 +14,6 @@ defmodule LiveViewStudioWeb.ServersLive do
     socket =
       assign(socket,
         servers: servers,
-        new_server_form: %Server{} |> Servers.change_server() |> to_form,
         coffees: 0
       )
 
@@ -27,16 +26,27 @@ defmodule LiveViewStudioWeb.ServersLive do
     {:noreply,
      assign(socket,
        selected_server: server,
-       page_title: server.name
+       page_title: server.name,
+       new_server_form: nil
      )}
   end
 
   def handle_params(_params, _uri, socket) do
-    {:noreply,
-     assign(socket,
-       selected_server: hd(socket.assigns.servers),
-       page_title: hd(socket.assigns.servers).name
-     )}
+    if socket.assigns.live_action == :new do
+      {:noreply,
+       assign(socket,
+         selected_server: nil,
+         page_title: nil,
+         new_server_form: %Server{} |> Servers.change_server() |> to_form
+       )}
+    else
+      {:noreply,
+       assign(socket,
+         selected_server: hd(socket.assigns.servers),
+         page_title: hd(socket.assigns.servers).name,
+         new_server_form: nil
+       )}
+    end
   end
 
   def render(assigns) do
@@ -45,6 +55,9 @@ defmodule LiveViewStudioWeb.ServersLive do
     <div id="servers">
       <div class="sidebar">
         <div class="nav">
+          <.link patch={~p"/servers/new"} class="add">
+            + Add New Server
+          </.link>
           <!-- `.link patch` meant for going to the same liveview and process -->
           <.link
             :for={server <- @servers}
@@ -64,31 +77,11 @@ defmodule LiveViewStudioWeb.ServersLive do
       </div>
       <div class="main">
         <div class="wrapper">
-          <div>
-            <.form for={@new_server_form} phx-submit="server-create">
-              <div class="field">
-                <label for="server_name">Server Name</label>
-                <.input field={@new_server_form[:name]} autocomplete="off" />
-              </div>
-              <div class="field">
-                <label for="server_framework">Framework</label>
-                <.input field={@new_server_form[:framework]} />
-              </div>
-              <div class="field">
-                <label for="server_size">Size in MB</label>
-                <.input
-                  field={@new_server_form[:size]}
-                  type="number"
-                  step="any"
-                />
-              </div>
-              <.button phx-disable-with="Saving...">
-                Add Server
-              </.button>
-            </.form>
-          </div>
-
-          <.server server={@selected_server} />
+          <%= if @live_action == :new do %>
+            <.new_server_form form={@new_server_form} />
+          <% else %>
+            <.server server={@selected_server} />
+          <% end %>
           <div class="links">
             <!-- `.link navigate` meant for going to a different liveview -->
             <.link navigate={~p"/light"}>Adjust Lights</.link>
@@ -120,8 +113,12 @@ defmodule LiveViewStudioWeb.ServersLive do
            new_server_form: %Server{} |> Servers.change_server() |> to_form
          )
          # Display the server that was just added
-         |> push_patch(to: ~p"/servers?#{server}")}
+         |> push_patch(to: ~p"/servers/#{server}")}
     end
+  end
+
+  def handle_event("cancel-server-create", _params, socket) do
+    {:noreply, socket |> push_patch(to: ~p"/servers")}
   end
 
   attr :server, :map, required: true
@@ -152,6 +149,33 @@ defmodule LiveViewStudioWeb.ServersLive do
           <%= @server.last_commit_message %>
         </blockquote>
       </div>
+    </div>
+    """
+  end
+
+  def new_server_form(assigns) do
+    ~H"""
+    <div class="server">
+      <.form for={@form} phx-submit="server-create">
+        <div class="field">
+          <label for="server_name">Server Name</label>
+          <.input field={@form[:name]} autocomplete="off" />
+        </div>
+        <div class="field">
+          <label for="server_framework">Framework</label>
+          <.input field={@form[:framework]} />
+        </div>
+        <div class="field">
+          <label for="server_size">Size in MB</label>
+          <.input field={@form[:size]} type="number" step="any" />
+        </div>
+        <.button phx-disable-with="Saving...">
+          Add Server
+        </.button>
+        <.link phx-click="cancel-server-create" class="cancel">
+          Cancel
+        </.link>
+      </.form>
     </div>
     """
   end
--- live_view_studio/lib/live_view_studio_web/router.ex
+++ live_view_studio/lib/live_view_studio_web/router.ex
@@ -24,6 +24,7 @@ defmodule LiveViewStudioWeb.Router do
     live "/flights", FlightsLive
     live "/boats", BoatsLive
     live "/servers", ServersLive
+    live "/servers/new", ServersLive, :new
     live "/servers/:id", ServersLive
     live "/donations", DonationsLive
     live "/volunteers", VolunteersLive
