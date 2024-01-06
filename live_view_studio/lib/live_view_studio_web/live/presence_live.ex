53deb6f54cd2568a576aa4a3d66c56ef2635890e
Jay Geeseman <geesemanj@gmail.com>
2024-01-06 06:40:17 +0000

    Add basic presence tracking to PresenceLive

--- live_view_studio/lib/live_view_studio_web/live/presence_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/presence_live.ex
@@ -1,20 +1,50 @@
 defmodule LiveViewStudioWeb.PresenceLive do
   use LiveViewStudioWeb, :live_view
 
+  alias LiveViewStudioWeb.Presence
+
+  @topic "users:video"
+
   def mount(_params, _session, socket) do
+    %{current_user: current_user} = socket.assigns
+
+    if connected?(socket) do
+      {:ok, _} =
+        Presence.track(self(), @topic, current_user.id, %{
+          username: current_user.email |> String.split("@") |> hd(),
+          is_playing: false
+        })
+    end
+
     socket =
       socket
       |> assign(:is_playing, false)
+      |> assign(:presences, simple_presence_map(Presence.list(@topic)))
 
     {:ok, socket}
   end
 
+  def simple_presence_map(presences) do
+    presences
+    |> Enum.into(%{}, fn {user_id, user_data} -> {user_id, hd(user_data[:metas])} end)
+  end
+
   def render(assigns) do
     ~H"""
+    <pre><%#= inspect(@presences, pretty: true) %></pre>
     <div id="presence">
       <div class="users">
         <h2>Who's Here?</h2>
-        <ul></ul>
+        <ul>
+          <li :for={{_user_id, user_data} <- @presences}>
+            <span class="status">
+              <%= if user_data.is_playing, do: "👀", else: "🙈" %>
+            </span>
+            <span class="username">
+              <%= user_data.username %>
+            </span>
+          </li>
+        </ul>
       </div>
       <div class="video" phx-click="toggle-playing">
         <%= if @is_playing do %>
