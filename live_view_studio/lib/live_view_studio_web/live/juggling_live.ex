82710b6ad1918c8e969c3d4db7f4583c15066cf6
Jay Geeseman <geesemanj@gmail.com>
2024-01-07 07:42:45 +0000

    Add key events for k, left, and right

--- live_view_studio/lib/live_view_studio_web/live/juggling_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/juggling_live.ex
@@ -19,7 +19,7 @@ defmodule LiveViewStudioWeb.JugglingLive do
   def render(assigns) do
     ~H"""
     <h1>Juggling Key Events</h1>
-    <div id="juggling">
+    <div id="juggling" phx-window-keyup="update">
       <div class="legend">
         k = play/pause, &larr; = previous, &rarr; = next
       </div>
@@ -43,6 +43,22 @@ defmodule LiveViewStudioWeb.JugglingLive do
 
   def handle_event("toggle-playing", _, socket) do
     {:noreply, toggle_playing(socket)}
+  end
+
+  def handle_event("update", %{"key" => "k"}, socket) do
+    {:noreply, toggle_playing(socket)}
+  end
+
+  def handle_event("update", %{"key" => "ArrowLeft"}, socket) do
+    {:noreply, assign(socket, :current, previous(socket))}
+  end
+
+  def handle_event("update", %{"key" => "ArrowRight"}, socket) do
+    {:noreply, assign(socket, :current, next(socket))}
+  end
+
+  def handle_event("update", _, socket) do
+    {:noreply, socket}
   end
 
   def handle_info(:tick, socket) do
